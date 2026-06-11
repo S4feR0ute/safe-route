6c9745d8483c9b027680c65fc629f48b74ba1ac4
AlbeCamp21 <luis.alanya.c@uni.pe>
2026-06-11 16:27:33 -0500

    feat(SAF-38): agregar clickeo de puntos en mapa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,36 @@
+import { useState } from 'react';
 import MapView from './components/MapView';
 import RouteForm from './components/RouteForm';
 
 function App() {
+  const [origen, setOrigen] = useState(null);
+  const [destino, setDestino] = useState(null);
+
+  const handleSelectPoint = (type, coords) => {
+    if (type === 'origen') {
+      setOrigen(coords);
+    } else if (type === 'destino') {
+      setDestino(coords);
+    }
+  };
+
+  const clearPoints = () => {
+    setOrigen(null);
+    setDestino(null);
+  };
+
   return (
-    // Se agrega position: relative para que el position: absolute del formulario funcione bien
     <div style={{ position: 'relative', height: '100vh', width: '100%' }}>
-      
-      
-      <RouteForm/>
-      
-      
-      <MapView/>
-      
+      <RouteForm 
+        origen={origen} 
+        destino={destino}
+        onClear={clearPoints}
+      />
+      <MapView 
+        origen={origen}
+        destino={destino}
+        onSelectPoint={handleSelectPoint}
+      />
     </div>
   );
 }
